fa17576e81aa201135acca08d00ad02c39365efc
PaulinhoMachadooo <paulinhomachadoo20@gmail.com>
2024-12-21 18:16:36 -0300

    Sobre

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image} from "react-native";
 
 export default function Screen() {
     return (
         <View style={styles.container}>
-            <Text>Fui eu quem fez :D</Text>
+            <Image style={{width:280, height:100}}
+                source={require('../../assets/guia-logo.png')}
+            />
+
+        
+            <View style={{flexDirection:"row", padding:5, width:'80%'}}>
+                 <Text style={{fontSize:20, fontWeight:"300", textAlign:'center'}}>O GUIA + é um projeto que foi densenvolvido para ajudar nas buscas por estabelecimentos e serviços da nossa cidade,
+                    assim também como a divulgar os comércios e prestadores de serviços.    
+                 </Text>
+            </View>
         </View>
     );
 }
@@ -13,6 +22,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        marginBottom:10,
     },
 });
